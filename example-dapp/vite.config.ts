adf7538b3bfc5c06414091f76c1f1bc894d16271
spencercap <cappiello.spencer@gmail.com>
2023-02-09 23:07:53 +0000

    added some pkgHelper constants

--- example-dapp/vite.config.ts
+++ example-dapp/vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import { clientPkgs, excludeClients, PROVIDER_ID } from '@thencc/algonautjs';
+
+console.log('clientPkgs', clientPkgs);
+const allClientPkgNames = Object.values(clientPkgs);
+// const allClientPkgNames = excludeClients([]);
+console.log('allClientPkgNames', allClientPkgNames);
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [vue()],
+
+	optimizeDeps: {
+		// for vite server to work correctly...
+		exclude: [
+			...allClientPkgNames,
+		],
+	},
+
+	build: {
+		minify: false,
+
+		rollupOptions: {
+			// THIS is how it works, but have to do the OPPOSITE of what you want
+			external: [
+				...excludeClients([
+					// PROVIDER_ID.INKEY,
+					PROVIDER_ID.PERA,
+					// PROVIDER_ID.MYALGO
+				]),
+
+			]
+		}
+	}
 })
--- src/utils/pkgHelpers.ts
+++ src/utils/pkgHelpers.ts
@@ -1,17 +1,19 @@
 import { PROVIDER_ID } from "../types";
 
 // maps client id to npm pkg
-export const clientPkgs = {
+export const CLIENT_PKG_MAP = {
 	[PROVIDER_ID.PERA]: '@perawallet/connect',
 	[PROVIDER_ID.MYALGO]: '@randlabs/myalgo-connect',
 	[PROVIDER_ID.INKEY]: '@thencc/inkey-client-js',
-}
+};
+export const CLIENT_IDS = Object.keys(CLIENT_PKG_MAP);
+export const CLIENT_PKGS = Object.values(CLIENT_PKG_MAP);
 
 export const excludeClients = (idsToDisable: PROVIDER_ID[]) => {
 	// console.log('excludeClients', idsToDisable);
 	let pkgsToDisable: string[] = [];
 	for (let id of idsToDisable) {
-		pkgsToDisable.push(clientPkgs[id]);
+		pkgsToDisable.push(CLIENT_PKG_MAP[id]);
 	}
 	return pkgsToDisable;
 }
