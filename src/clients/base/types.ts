262f5a4d7f77aee60b9b7fc3d2c9c4b152007d87
spencer cap <spencercap@users.noreply.github.com>
2023-06-21 01:26:42 +0100

    removed WalletProvider type

--- src/clients/base/types.ts
+++ src/clients/base/types.ts
@@ -16,4 +16,3 @@ export type ClientInitParams = {
 export type ClientConstructorParams = {
 	someField?: string;
 };
-
--- src/types/shared.ts
+++ src/types/shared.ts
@@ -9,20 +9,3 @@ export interface Account {
 	// timeAdded: number; // datetime as number
 	// timeLastUsed: number;
 }
-
-export type WalletProvider = {
-	id: W_ID;
-	name: string;
-	icon: string;
-	chain: string;
-};
-
-type ExtendValues<Type> = {
-	[Property in keyof Type]: Type[Property] | null;
-};
-
-// TODO rename this type as its very misleading
-// This type extends the values of `WalletProvider` with `null` values and adds the `accounts` property.
-// export type WalletAccounts = ExtendValues<WalletProvider> & {
-// 	accounts: Account[];
-// };
